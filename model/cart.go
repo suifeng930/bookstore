54792ec8e69703c248c71031d796c791d7bb1d1a
tom_glb <goodMorning_glb@atguigu.com>
2020-08-12 22:09:12 +0800

    完成购物车的结构体设计

---
+++ model/cart.go
@@ -0,0 +1,31 @@
+package model
+
+//cart 购物车
+
+type Cart struct {
+	CartId      string      //购物车id
+	CartItems   []*CartItem //购物车中所有的购物项
+	TotalCount  int64       // 购物车中图书的总数量，通过计算得到
+	TotalAmount float64     //购物车中图书的总金额，通过计算得到
+	UserId      int         // 购物车用户id
+}
+
+// 获取购物车中图书的总数量
+func (cart *Cart) GetTotalCount() int64 {
+	var totalCount int64
+	//遍历购物车中的购物项切片
+	for _, value := range cart.CartItems {
+		totalCount = totalCount + value.Count
+	}
+	return totalCount
+}
+
+// 获取购物车中图书的总金额
+func (cart *Cart) GetTotalAmount() float64 {
+	var totalAmount float64
+	//遍历购物车中的购物项切片
+	for _, value := range cart.CartItems {
+		totalAmount = totalAmount + value.Amount
+	}
+	return totalAmount
+}
---
+++ model/cartitem.go
@@ -0,0 +1,17 @@
+package model
+
+//购物项
+type CartItem struct {
+	CartItemId int64   //购物项id
+	Book       *Book   //购物项中的图书信息
+	Count      int64   //购物项中图书的数量
+	Amount     float64 // 购物项中图书的金额小计，通过计算得到
+	CartId     string  //购物车Id
+}
+
+//GetAmou
+func (cartItem *CartItem) GetAmount() float64 {
+	price := cartItem.Book.Price
+	return float64(cartItem.Count) * price
+
+}
